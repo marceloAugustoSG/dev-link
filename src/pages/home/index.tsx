ff66e85fa58d18c79da38f1bed6841136009f32b
marceloAugustoSG <marcelo_augusto.soares@hotmail.com>
2025-09-03 22:39:57 -0300

    configurando o tailwind

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
     return (
-        <h1>Página HOME</h1>
+        <div className="w-full h-screen bg-neutral-900">
+
+            <h1 className="text-amber-50 text-3xl">Página HOME</h1>
+
+        </div>
     )
 }

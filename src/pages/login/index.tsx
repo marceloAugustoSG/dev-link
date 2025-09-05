bf712fba096da23e7d99d7231cabae5fa3cafe5a
marceloAugustoSG <marcelo_augusto.soares@hotmail.com>
2025-09-05 14:21:10 -0300

    finalizando tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { useState, type FormEvent } from "react";
 
 export default function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        console.log({ email, password });
+
+
+    }
     return (
         <div className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
@@ -8,7 +20,24 @@ export default function Login() {
                     <span className=" bg-gradient-to-r from-yellow-500 to-orange-400 bg-clip-text text-transparent" >Link </span>
                 </h1>
             </Link>
-
+            <form onSubmit={handleSubmit}
+                className="w-full max-w-xl flex flex-col px-2"
+            >
+                <Input
+                    placeholder="Digite o seu Email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    placeholder="********"
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white"
+                    type="submit">Acessar</button>
+            </form>
 
         </div>
     )

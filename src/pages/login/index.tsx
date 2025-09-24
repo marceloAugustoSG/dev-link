83ce88847bdb5508a84d5a63524bf30bd8e1e1b9
marceloAugustoSG <marcelo_augusto.soares@hotmail.com>
2025-09-24 18:55:29 -0300

    iniciando tela de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
-    return <div>Página Login</div>
+    return <div className="flex w-full h-screen items-center justify-center flex-col">
+        <Link to="/">
+            <h1 className="mt-11 text-white mb-7 font-bold text-5xl">
+                Dev<span className="bg-gradient-to-r from-yellow-500 to bg-orange-400 bg-clip-text text-transparent">Link</span>
+            </h1>
+        </Link>
+    </div>
 }
